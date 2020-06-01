45551ef209be426458b1c41aee5efd3626c6a58a
Jacob Silver <jacobsilver2@mac.com>
2020-06-01 14:41:13 -0400

    still sorting out merge stuff

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,11 +6,7 @@ import { ThemeProvider, createGlobalStyle } from "styled-components"
 import Header from "./header"
 import { VideoBackground, VideoForeground } from "../styles/VideoPlayer"
 import ContentContainer from "../styles/ContentContainer"
-<<<<<<< HEAD
-import { GlobalStateContext } from "../context/provider"
-=======
 import { GlobalStateContext, GlobalDispatchContext } from "../context/provider"
->>>>>>> 0.01
 // import "./layout.css"
 
 export const theme = {
@@ -55,10 +51,7 @@ const GlobalStyle = createGlobalStyle`
 
 const Layout = ({ children }) => {
   const state = useContext(GlobalStateContext)
-<<<<<<< HEAD
-=======
   const dispatch = useContext(GlobalDispatchContext)
->>>>>>> 0.01
   const videoRef = useRef()
   const youTubeOptions = {
     playerVars: {
@@ -77,8 +70,6 @@ const Layout = ({ children }) => {
   const onEnd = e => {
     e.target.playVideo()
   }
-<<<<<<< HEAD
-=======
 
   const handleSoundClick = () => {
     dispatch({ type: "TOGGLE_SOUND" })
@@ -87,7 +78,6 @@ const Layout = ({ children }) => {
       : videoRef.current.internalPlayer.mute()
   }
 
->>>>>>> 0.01
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -103,13 +93,9 @@ const Layout = ({ children }) => {
       <GlobalStyle />
       <Header
         siteTitle={data.site.siteMetadata.title}
-<<<<<<< HEAD
-        ytplayer={videoRef.current}
-=======
         // ytplayer={videoRef.current}
         handleSoundClick={handleSoundClick}
         ref={videoRef}
->>>>>>> 0.01
       />
       <VideoBackground>
         <VideoForeground>
@@ -123,13 +109,9 @@ const Layout = ({ children }) => {
         </VideoForeground>
       </VideoBackground>
       <ContentContainer>
-<<<<<<< HEAD
-        <main>{children}</main>
-=======
         <div></div>
         {children}
         <div></div>
->>>>>>> 0.01
       </ContentContainer>
     </ThemeProvider>
   )
--- src/styles/ContentContainer.js
+++ src/styles/ContentContainer.js
@@ -1,11 +1,5 @@
 import styled from "styled-components"
 
-<<<<<<< HEAD
-const ContentContainer = styled.div`
-  margin: 0 auto;
-  max-width: 960px;
-  padding: 0 1.0875rem 1.45rem;
-=======
 const ContentContainer = styled.main`
   margin: 0 auto;
   max-width: 960px;
@@ -14,7 +8,6 @@ const ContentContainer = styled.main`
   grid-template-rows: 1fr 1fr 1fr;
   /* align-items: center; */
   /* justify-content: center; */
->>>>>>> 0.01
 `
 
 export default ContentContainer
