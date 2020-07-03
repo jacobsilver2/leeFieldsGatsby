62a3ca2588630de691754ae0f3047ef026767a78
Jacob Silver <jacobsilver2@mac.com>
2020-07-03 17:09:02 -0400

    trying out not using fonts file, hopefully to fix FOUC

--- rootWrapper.js
+++ rootWrapper.js
@@ -3,14 +3,14 @@ import GlobalContextProvider from "./src/context/provider"
 import Layout from "./src/components/layout"
 import { ThemeProvider } from "styled-components"
 import GlobalStyle from "./src/styles/GlobalStyle"
-import Fonts from "./src/styles/Fonts"
+// import Fonts from "./src/styles/Fonts"
 import { theme } from "./src/styles/theme"
 
 export const wrapRootElement = ({ element }) => {
   return (
     <GlobalContextProvider>
       <ThemeProvider theme={theme}>
-        <Fonts />
+        {/* <Fonts /> */}
         <GlobalStyle />
         <Layout>{element}</Layout>
       </ThemeProvider>
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,20 @@
 import { createGlobalStyle } from "styled-components"
 import reset from "styled-reset"
+import FormaDJRBannerBoldWoff from "../assets/fonts/FormaDJRBanner-Bold-Testing.woff"
+import FormaDJRBannerBoldWoff2 from "../assets/fonts/FormaDJRBanner-Bold-Testing.woff2"
+import FormaDJRBannerBoldEOT from "../assets/fonts/FormaDJRBanner-Bold-Testing.eot"
+
+import FormaDJRBannerMedWoff from "../assets/fonts/FormaDJRBanner-Medium-Testing.woff"
+import FormaDJRBannerMedWoff2 from "../assets/fonts/FormaDJRBanner-Medium-Testing.woff2"
+import FormaDJRBannerMedEOT from "../assets/fonts/FormaDJRBanner-Medium-Testing.eot"
+
+import FormaDJRTextRegWoff from "../assets/fonts/FormaDJRText-Regular-Testing.woff"
+import FormaDJRTextRegWoff2 from "../assets/fonts/FormaDJRText-Regular-Testing.woff2"
+import FormaDJRTextRegEOT from "../assets/fonts/FormaDJRText-Regular-Testing.eot"
+
+import FormaDJRTextMedWoff from "../assets/fonts/FormaDJRText-Medium-Testing.woff"
+import FormaDJRTextMedWoff2 from "../assets/fonts/FormaDJRText-Medium-Testing.woff2"
+import FormaDJRTextMedEOT from "../assets/fonts/FormaDJRBanner-Medium-Testing.eot"
 // import FormaDJRBannerBold from "../assets/fonts/FormaDJRBanner-Bold-Testing.woff2"
 // import FormaDJRBannerMed from "../assets/fonts/FormaDJRBanner-Medium-Testing.woff2"
 // import FormaDJRTextReg from "../assets/fonts/FormaDJRText-Regular-Testing.woff2"
@@ -10,6 +25,32 @@ const GlobalStyle = createGlobalStyle`
   *, *:before, *:after {
     box-sizing: border-box;
   }
+
+  @font-face {
+    font-family: ${({ theme }) => theme.font.banner};
+    src: url(${FormaDJRBannerBoldWoff2}) format('woff2'),
+         url(${FormaDJRBannerBoldWoff}) format('woff'),
+         url(${FormaDJRBannerBoldEOT});
+  }
+  @font-face {
+    font-family: ${({ theme }) => theme.font.bannerMedium};
+    src: url(${FormaDJRBannerMedWoff2}) format('woff2'),
+         url(${FormaDJRBannerMedWoff}) format('woff'),
+         /* url(${FormaDJRBannerMedEOT}); */
+  }
+  @font-face {
+    font-family: ${({ theme }) => theme.font.text};
+    src: url(${FormaDJRTextRegWoff2}) format('woff2'),
+         url(${FormaDJRTextRegWoff}) format('woff'),
+         url(${FormaDJRTextRegEOT});
+  }
+  @font-face {
+    font-family: ${({ theme }) => theme.font.bold};
+    src: url(${FormaDJRTextMedWoff2}) format('woff2'),
+         url(${FormaDJRTextMedWoff}) format('woff'),
+         url(${FormaDJRTextMedEOT});
+  }
+  
   html {
     box-sizing: border-box;
     /* font-family: ${({ theme }) => theme.font.banner}; */
