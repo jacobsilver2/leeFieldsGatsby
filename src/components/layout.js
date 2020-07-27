f22e3135b1d9a56ebc671712fa283cc26ae3b4a3
Jacob Silver <jacobsilver2@mac.com>
2020-07-27 15:32:24 -0400

    fixed homepage/header/video z-index bug

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React, { useContext, useRef, useEffect, useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { GlobalDispatchContext, GlobalStateContext } from "../context/provider"
-import { useOnClickOutside } from "../hooks/useOnClickOutside"
+// import { useOnClickOutside } from "../hooks/useOnClickOutside"
 import Headroom from "react-headroom"
 import Header from "./header"
 import Footer from "./footer"
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
   const dispatch = useContext(GlobalDispatchContext)
   const state = useContext(GlobalStateContext)
   const menuNode = useRef()
-  useOnClickOutside(menuNode, () => dispatch({ type: "SECONDARY_MENU_OFF" }))
+  // useOnClickOutside(menuNode, () => dispatch({ type: "SECONDARY_MENU_OFF" }))
 
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -40,7 +40,10 @@ const Layout = ({ children }) => {
     <>
       <Wrapper>
         {state.videoModalOpen && <VideoModalComponent />}
-        <Headroom onUnpin={() => dispatch({ type: "SECONDARY_MENU_OFF" })}>
+        <Headroom
+          style={{ zIndex: "1000" }}
+          onUnpin={() => dispatch({ type: "SECONDARY_MENU_OFF" })}
+        >
           <Header siteTitle={data.site.siteMetadata.title} />
         </Headroom>
         <ContentContainer>{children}</ContentContainer>
