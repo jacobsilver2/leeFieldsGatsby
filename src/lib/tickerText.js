de64c2b0c352409007d8fb022b78df9679f766b7
Jacob Silver <jacobsilver2@mac.com>
2020-11-03 14:32:11 -0500

    changed the ticker content

--- src/lib/tickerText.js
+++ src/lib/tickerText.js
@@ -1,6 +1,6 @@
 const tickerText = {
-  text: `NEW ALBUM "IT RAINS LOVE" OUT NOW ON BIG CROWN RECORDS`,
-  link: "http://www.bigcrownrecords.com",
+  text: `Lee Fields & The Expressions - Big Crown Vaults Vol. 1 out 12/4`,
+  link: "https://bigcrownrecords.com/store/vaults-vol-1-lee-fields/",
 }
 
 export default tickerText
